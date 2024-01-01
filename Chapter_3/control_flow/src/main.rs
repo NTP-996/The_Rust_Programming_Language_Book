23f88e21b9c9162fb2b688e4e09e5f526bf3e6c3
peter <thaiphuongn1@gmail.com>
2024-01-01 15:42:17 +0700

    Chapter 4

--- Chapter_3/control_flow/src/main.rs
+++ Chapter_3/control_flow/src/main.rs
@@ -33,4 +33,77 @@ fn main() {
     //    let number = if condition { 5 } else { 6 };
 
     //    println!("The value of number is: {number}");
+
+    //   let condition = true;
+
+    //    let number = if condition { 5 } else { "six" };
+
+    //    println!("The value of number is: {number}");
+
+    //    loop {
+    //        println!("again!");
+    //    }
+
+    //    let mut counter = 0;
+    //
+    //    let result = loop {
+    //        counter += 1;
+    //        if counter == 10 {
+    //            break counter * 2;
+    //        }
+    //    };
+    //
+    //    println!("The result is {result}");
+
+    // Loop Labels to Disambiguate Between Multiple Loops
+    //    let mut count = 0;
+    //    'counting_up: loop {
+    //        println!("count = {count}");
+    //        let mut remaining = 10;
+    //
+    //        loop {
+    //            println!("remaining = {remaining}");
+    //            if remaining == 9 {
+    //                break;
+    //            }
+    //            if count == 2 {
+    //                break 'counting_up;
+    //            }
+    //            remaining -= 1;
+    //        }
+    //        count += 1;
+    //    }
+    //    println!("End count = {count}");
+
+    // Conditional Loops with while
+
+    //    let mut number = 3;
+    //
+    //    while number != 0 {
+    //        println!("{number}!");
+    //
+    //        number -= 1;
+    //    }
+    //
+    //    println!("LIFTOFF!!!");
+
+    //    let a = [10, 20, 30, 40, 50];
+    //    let mut index = 0;
+    //
+    //    while index < 5 {
+    //        println!("the value is: {}", a[index]);
+    //
+    //        index += 1;
+    //    }
+
+    //    let a = [10, 20, 30, 40, 50];
+    //
+    //    for element in a {
+    //        println!("the value is: {element}");
+    //    }
+
+    for number in (1..4).rev() {
+        println!("{number}");
+    }
+    println!("LIFTOFF!!!");
 }
