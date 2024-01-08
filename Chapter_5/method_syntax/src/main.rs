fcd89c830c01e1bad880162dcc008d13ac736f2a
peter <thaiphuongn1@gmail.com>
2024-01-08 16:41:31 +0700

    medthod

--- Chapter_5/method_syntax/src/main.rs
+++ Chapter_5/method_syntax/src/main.rs
@@ -10,6 +10,21 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -18,8 +33,25 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
